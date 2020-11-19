20ef739f8dae6c0459d27e62ab8c992b9df51800
joaoalveszup <joao.alves@zup.com.br>
2020-11-19 15:18:28 -0300

    Vamos começar a brincadeira.

---
+++ src/main/java/br/com/zup/estrelas/sme/SolucaoMicroEmpreendedorApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.sme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolucaoMicroEmpreendedorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolucaoMicroEmpreendedorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/estrelas/sme/SolucaoMicroEmpreendedorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.sme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolucaoMicroEmpreendedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
