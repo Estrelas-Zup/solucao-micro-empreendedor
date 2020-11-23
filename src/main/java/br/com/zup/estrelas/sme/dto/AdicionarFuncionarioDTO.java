1f46b3a42e49689c1b808038e5b909fc1ac16584
Paulo Sousa <paulo.sousa@zup.com.br>
2020-11-23 18:08:20 -0300

    criacao de crud funcionario na service

---
+++ src/main/java/br/com/zup/estrelas/sme/dto/AdicionarFuncionarioDTO.java
@@ -0,0 +1,98 @@
+package br.com.zup.estrelas.sme.dto;
+
+import java.time.LocalDate;
+
+public class AdicionarFuncionarioDTO {
+
+    private String nome;
+
+    private String cpf;
+
+    private String endereco;
+
+    private LocalDate dataNascimento;
+
+    private String telefone;
+
+    private String email;
+
+    private String cargo;
+
+    private String salario;
+
+    private String numeroCarteiraTrabalho;
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getSalario() {
+        return salario;
+    }
+
+    public void setSalario(String salario) {
+        this.salario = salario;
+    }
+
+    public String getNumeroCarteiraTrabalho() {
+        return numeroCarteiraTrabalho;
+    }
+
+    public void setNumeroCarteiraTrabalho(String numeroCarteiraTrabalho) {
+        this.numeroCarteiraTrabalho = numeroCarteiraTrabalho;
+    }
+
+}
---
+++ src/main/java/br/com/zup/estrelas/sme/service/FuncionarioService.java
@@ -0,0 +1,23 @@
+package br.com.zup.estrelas.sme.service;
+
+import java.util.List;
+import br.com.zup.estrelas.sme.dto.AdicionarFuncionarioDTO;
+import br.com.zup.estrelas.sme.dto.AlteraFuncionarioDTO;
+import br.com.zup.estrelas.sme.dto.MensagemDTO;
+import br.com.zup.estrelas.sme.entity.Funcionario;
+
+public interface FuncionarioService {
+
+    public MensagemDTO adicionarFuncionario(AdicionarFuncionarioDTO adicionarFuncionarioDTO);
+
+    public MensagemDTO alterarFuncionario(Long idFUncionario, AlteraFuncionarioDTO alteraFuncionarioDTO);
+
+    public Funcionario consultarFuncionarioPorCpf(String cpf);
+
+    public List<Funcionario> listarFuncionarios();
+
+    public MensagemDTO removerFuncionario(String cpf);
+    
+    public MensagemDTO verificarDisponibilidadeContratacao(Double salario);
+
+}
