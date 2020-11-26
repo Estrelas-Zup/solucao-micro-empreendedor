9433ce3a53aa418156b68dc5134a49d007667d86
andersoncanciozup <anderson.cancio@zup.com.br>
2020-11-26 16:07:36 -0300

    Implementação de validações de entradas entidade e DTO que interagem com estoque

--- src/main/java/br/com/zup/estrelas/sme/dto/EstoqueDTO.java
+++ src/main/java/br/com/zup/estrelas/sme/dto/EstoqueDTO.java
@@ -1,10 +1,16 @@
 package br.com.zup.estrelas.sme.dto;
 
 import java.time.LocalDate;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 
 public class EstoqueDTO {
+    @PositiveOrZero(message = "IdProduto deve ser maior que zero!")
     private long idProduto;
+    @Positive(message = "Quantidade não deve ser negativa!")
     private int quantidade;
+    @FutureOrPresent(message = "Data de validade não deve ser menor que data atual")
     private LocalDate dataValidade;
 
     public EstoqueDTO() {
