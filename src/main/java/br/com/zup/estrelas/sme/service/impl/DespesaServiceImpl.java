7904db1e47b7bce84be8066ae8123574a72043a2
unknown <elias.borges@zup.com.br>
2020-11-26 00:09:37 -0300

    Refatoracao em metodos de despesa na service

--- src/main/java/br/com/zup/estrelas/sme/service/impl/DespesaServiceImpl.java
+++ src/main/java/br/com/zup/estrelas/sme/service/impl/DespesaServiceImpl.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.zup.estrelas.sme.dto.DespesaDTO;
 import br.com.zup.estrelas.sme.dto.MensagemDTO;
+import br.com.zup.estrelas.sme.entity.Caixa;
 import br.com.zup.estrelas.sme.entity.Despesa;
+import br.com.zup.estrelas.sme.repository.CaixaRepository;
 import br.com.zup.estrelas.sme.repository.DespesaRepository;
 import br.com.zup.estrelas.sme.service.DespesaService;
 
@@ -15,46 +17,50 @@ import br.com.zup.estrelas.sme.service.DespesaService;
 public class DespesaServiceImpl implements DespesaService {
 
     private static final String DESPESA_CADASTRADA_COM_SUCESSO = "Despesa cadastrada com sucesso!";
-    private static final String DESPESA_INEXISTENTE = "Despesa inexistente!";
+    private static final String DESPESA_INEXISTENTE =
+            "Não foi possivel realizar a operação, despesa inexistente.";
     private static final String DESPESA_REMOVIDA_COM_SUCESSO = "Despesa removida com sucesso!";
     private static final String DESPESA_ALTERADA_COM_SUCESSO = "Despesa alterada com sucesso!";
 
     @Autowired
     DespesaRepository despesaRepository;
 
-    public MensagemDTO adicionarDespesa(DespesaDTO despesaDTO) {
+    @Autowired
+    CaixaRepository caixaRepository;
 
+    public MensagemDTO adicionarDespesa(DespesaDTO despesaDTO) {
         Despesa despesa = new Despesa();
+
+        Caixa caixa = caixaRepository.findFirstByOrderByIdCaixaDesc();
 
         BeanUtils.copyProperties(despesaDTO, despesa);
 
+        despesa.setCaixa(caixa);
         despesaRepository.save(despesa);
         return new MensagemDTO(DESPESA_CADASTRADA_COM_SUCESSO);
+    }
+
+    public MensagemDTO alterarDespesa(Long idDespesa, DespesaDTO despesaDTO) {
+        Optional<Despesa> despesaProcurada = despesaRepository.findById(idDespesa);
+
+        if (despesaProcurada.isEmpty()) {
+            return new MensagemDTO(DESPESA_INEXISTENTE);
+        }
+
+        Despesa despesaAlterada = despesaProcurada.get();
+        BeanUtils.copyProperties(despesaDTO, despesaAlterada);
+
+        despesaRepository.save(despesaAlterada);
+
+        return new MensagemDTO(DESPESA_ALTERADA_COM_SUCESSO);
+
     }
 
     public List<Despesa> listarDespesa() {
         return (List<Despesa>) despesaRepository.findAll();
     }
 
-    public MensagemDTO alterarDespesa(Long idDespesa, DespesaDTO despesaDTO) {
-
-        Optional<Despesa> despesaProcurada = despesaRepository.findById(idDespesa);
-
-        if (despesaProcurada.isPresent()) {
-
-            Despesa despesaAlterada = despesaProcurada.get();
-            BeanUtils.copyProperties(despesaDTO, despesaAlterada);
-
-            despesaRepository.save(despesaAlterada);
-
-            return new MensagemDTO(DESPESA_ALTERADA_COM_SUCESSO);
-        }
-
-        return new MensagemDTO(DESPESA_INEXISTENTE);
-    }
-
     public MensagemDTO removerDespesa(Long idDespesa) {
-
         if (despesaRepository.existsById(idDespesa)) {
             despesaRepository.deleteById(idDespesa);
             return new MensagemDTO(DESPESA_REMOVIDA_COM_SUCESSO);
