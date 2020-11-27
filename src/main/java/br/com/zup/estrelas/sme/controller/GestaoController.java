fd0ddfb095cbc83d671409b1468686a5c8bf3cbc
CarlosHenrique-zup <carlos.henrique@zup.com.br>
2020-11-27 18:24:01 -0300

    [Swagger] - anotacoes - venda

--- src/main/java/br/com/zup/estrelas/sme/controller/GestaoController.java
+++ src/main/java/br/com/zup/estrelas/sme/controller/GestaoController.java
@@ -9,15 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.zup.estrelas.sme.dto.AberturaComercioDTO;
 import br.com.zup.estrelas.sme.dto.MensagemDTO;
 import br.com.zup.estrelas.sme.service.GestaoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/gestao")
+@Api(value = "Gestão")
 public class GestaoController {
 
     @Autowired
     GestaoService gestaoService;
 
     @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ApiOperation(value = "Adicionar gestão")
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Criado com sucesso!"),
+            @ApiResponse(code = 500, message = "Erro interno no servidor")})
     public MensagemDTO aberturaComercio(@RequestBody AberturaComercioDTO aberturaComercioDTO) {
         return gestaoService.aberturaComercio(aberturaComercioDTO);
     }
