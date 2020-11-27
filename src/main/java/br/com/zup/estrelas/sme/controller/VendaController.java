95e64e66a1bd3eac26b8d01a4b6d20947ffbfcde
CarlosHenrique-zup <carlos.henrique@zup.com.br>
2020-11-27 17:57:11 -0300

    [Swagger] - Anotacao - Venda

--- src/main/java/br/com/zup/estrelas/sme/controller/VendaController.java
+++ src/main/java/br/com/zup/estrelas/sme/controller/VendaController.java
@@ -13,38 +13,63 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.zup.estrelas.sme.dto.AdicionarVendaDTO;
 import br.com.zup.estrelas.sme.dto.AlterarVendaDTO;
-import br.com.zup.estrelas.sme.entity.Venda;
 import br.com.zup.estrelas.sme.dto.MensagemDTO;
+import br.com.zup.estrelas.sme.entity.Venda;
 import br.com.zup.estrelas.sme.service.VendaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/vendas")
+@Api(value = "Venda")
 public class VendaController {
 
     @Autowired
     VendaService service;
 
     @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ApiOperation(value = "Adicionar venda")
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Criado com sucesso!"),
+            @ApiResponse(code = 500, message = "Erro interno no servidor")})
     public MensagemDTO adicionarVenda(@RequestBody AdicionarVendaDTO adicionarVendaDTO) {
         return service.adicionarVenda(adicionarVendaDTO);
     }
 
     @PutMapping(path = "/{idVenda}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public MensagemDTO alterarVenda(@PathVariable Long idVenda, @RequestBody AlterarVendaDTO alterarVendaDTO) {
+    @ApiOperation(value = "Alterar venda")
+    @ApiResponses(
+            value = {@ApiResponse(code = 200, message = "Alteração da venda feita com sucesso!"),
+                    @ApiResponse(code = 204, message = "Nenhuma venda alterada!")})
+    public MensagemDTO alterarVenda(@PathVariable Long idVenda,
+            @RequestBody AlterarVendaDTO alterarVendaDTO) {
         return service.alterarVenda(idVenda, alterarVendaDTO);
     }
 
     @GetMapping(path = "/{idVenda}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ApiOperation(value = "Buscar venda por ID")
+    @ApiResponses(
+            value = {@ApiResponse(code = 200, message = "Procura do ID da venda feita com sucesso!"),
+                    @ApiResponse(code = 204, message = "Nenhuma venda encontrada pelo ID!")})
     public Venda buscarVendaPorId(@PathVariable Long idVenda) {
         return service.buscarVendaPorId(idVenda);
     }
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ApiOperation(value = "Listar vendas")
+    @ApiResponses(
+            value = {@ApiResponse(code = 200, message = "Procura da venda feita com sucesso!"),
+                    @ApiResponse(code = 204, message = "Nenhuma venda encontrada!")})
     public List<Venda> listarVendas() {
         return service.listarVendas();
     }
-    
+
     @DeleteMapping(path = "/{idVenda}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ApiOperation(value = "Remover venda")
+    @ApiResponses(
+            value = {@ApiResponse(code = 200, message = "Remoção da venda feita com sucesso!"),
+                    @ApiResponse(code = 204, message = "Nenhuma venda removida!")})
     public MensagemDTO removerVenda(@PathVariable Long idVenda) {
         return service.removerVenda(idVenda);
     }
