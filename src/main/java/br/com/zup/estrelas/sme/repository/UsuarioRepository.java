4839754f66c3f666bbf96f7c06a83ec1012b3b69
Paulo Sousa <paulo.sousa@zup.com.br>
2020-11-30 16:06:07 -0300

    implementação das regras de negocio de usuario 0.3

--- src/main/java/br/com/zup/estrelas/sme/repository/UsuarioRepository.java
+++ src/main/java/br/com/zup/estrelas/sme/repository/UsuarioRepository.java
@@ -1,6 +1,5 @@
 package br.com.zup.estrelas.sme.repository;
 
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import br.com.zup.estrelas.sme.entity.Usuario;
@@ -8,7 +7,5 @@ import br.com.zup.estrelas.sme.entity.Usuario;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, String> {
-
-    Optional<Usuario> findByEmail(String email);
 
 }
