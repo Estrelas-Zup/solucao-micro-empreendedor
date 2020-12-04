d0b3129c1baf4104bc87957c8aea569a71138fcd
eliasborgeszup <elias.borges@zup.com.br>
2020-12-04 00:59:21 -0300

    Correção em validação capitalSocial em AberturaComercioDTO

--- src/main/java/br/com/zup/estrelas/sme/dto/AberturaComercioDTO.java
+++ src/main/java/br/com/zup/estrelas/sme/dto/AberturaComercioDTO.java
@@ -1,13 +1,14 @@
 package br.com.zup.estrelas.sme.dto;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
 
 public class AberturaComercioDTO {
-    
+    @DecimalMax(value = "81.000", message ="O campo não pode ter valor acima de R$81.000")
+    @DecimalMin(value = "1.0", message ="O campo não pode ter valor abaixo de R$1.0")
     @Positive(message = "O campo deve ser maior que zero.")
-    @Size(min = 1, max = 8100, message = "O campo deve conter entre R$1,00 a R$81.000,00.")
     @NotNull(message = "O campo não pode ser vazio.")
     private Double capitalSocial;
 
@@ -18,5 +19,5 @@ public class AberturaComercioDTO {
     public void setCapitalSocial(Double capitalSocial) {
         this.capitalSocial = capitalSocial;
     }
-    
+
 }
