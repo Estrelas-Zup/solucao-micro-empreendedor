9ef12a304e235f88fb15d162525288bd8f92ea34
eliasborgeszup <elias.borges@zup.com.br>
2020-12-05 07:42:49 -0300

    Criação de metodo adicionarInvestimentoCapitalSocial e validação em aberturaComercio

--- src/main/java/br/com/zup/estrelas/sme/dto/AberturaComercioDTO.java
+++ src/main/java/br/com/zup/estrelas/sme/dto/AberturaComercioDTO.java
@@ -1,13 +1,14 @@
 package br.com.zup.estrelas.sme.dto;
 
-import javax.validation.constraints.DecimalMax;
-import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
 public class AberturaComercioDTO {
-    @DecimalMax(value = "81.000", message ="O campo não pode ter valor acima de R$81.000")
-    @DecimalMin(value = "1.0", message ="O campo não pode ter valor abaixo de R$1.0")
+    /*
+     * @DecimalMax(value = "81.000", message ="O campo não pode ter valor acima de R$81.000")
+     * 
+     * @DecimalMin(value = "1.0", message ="O campo não pode ter valor abaixo de R$1.0")
+     */
     @Positive(message = "O campo deve ser maior que zero.")
     @NotNull(message = "O campo não pode ser vazio.")
     private Double capitalSocial;
