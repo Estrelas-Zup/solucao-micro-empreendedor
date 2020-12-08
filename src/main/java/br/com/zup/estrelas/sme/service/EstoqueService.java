dfd7243e8ef861493bfc135acaeaa5dcebf56596
eliasborgeszup <elias.borges@zup.com.br>
2020-12-08 17:14:47 -0300

    Adição de sub recursos em estoque

--- src/main/java/br/com/zup/estrelas/sme/service/EstoqueService.java
+++ src/main/java/br/com/zup/estrelas/sme/service/EstoqueService.java
@@ -8,13 +8,13 @@ import br.com.zup.estrelas.sme.entity.Estoque;
 
 public interface EstoqueService {
 
-    public MensagemDTO adicionarEstoque(EstoqueDTO estoqueDTO);
+    public MensagemDTO adicionarEstoque(Long idProduto, EstoqueDTO estoqueDTO);
 
-    public MensagemDTO alterarEstoque(Long idEstorque, EstoqueDTO estoqueDTO);
+    public MensagemDTO alterarEstoque(Long idProduto, Long idEstoque, EstoqueDTO estoqueDTO);
 
     public List<Estoque> consultarEstoquePorProduto(Long idProduto);
 
     public List<Estoque> listarEstoques();
     
-    public MensagemDTO contablizarPerda(Long idEstoque, ContabilizaPerdaDTO contabilizaPerdaDTO);
+    public MensagemDTO contablizarPerda(Long idProduto, Long idEstoque, ContabilizaPerdaDTO contabilizaPerdaDTO);
 }
